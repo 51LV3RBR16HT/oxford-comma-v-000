36b5ad36e8da0f00033b425dd0d05136a28de915
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-07 23:56:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,12 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size >= 3
-    array.join(", ").insert(-10, "and ")
+    last_array = "and #{array[-1]}"
+
+
+
+
+    # array.join(", ").insert(-10, "and ")
   # else array.size > 3
   #   array.join(", ").insert(-2, " and ")
 
