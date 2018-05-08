2c5092ac23d11e6a4c15a8e2eac003188efecbc1
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-08 00:00:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   # if array.size < 2
     array.join
-  elsif array.size == 2
+  if array.size == 2
     array.join(" and ")
-  elsif array.size >= 3
+  else array.size >= 3
     last_array = "and #{array[-1]}"
     array.pop
     array.push(last_array).join(", ")
